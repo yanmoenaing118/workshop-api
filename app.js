1e4f9a0de8fb9fcc6afcc27c8756108f366b440b
yanmoenaing118 <yan.webdev.1234@gmail.com>
2022-03-14 23:11:37 +0630

    start

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ app.use("/api/v1/dramas", dramaRouter);
 app.use("/api/v1/songs", songRouter)
 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('server running on port 3000');
 })
